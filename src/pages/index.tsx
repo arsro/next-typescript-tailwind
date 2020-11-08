b4bd023cfb366112cc551272510c4fd81a325ec6
arsro <arsro.dev@gmail.com>
2020-11-08 14:04:38 +0900

    fix style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next'
 
-const Page: NextPage = (props) => {
+const Page: NextPage = () => {
   return (
     <div className="bg-gray-200 p-4">
       <span className="block text-gray-700 text-center bg-gray-400 px-4 py-2">1</span>
